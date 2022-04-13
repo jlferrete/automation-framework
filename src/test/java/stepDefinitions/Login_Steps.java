a6e4af73664b6fa58ccce3be91a28a37a83b60b2
jlferrete <joseluisferrete@gmail.com>
2022-04-14 01:15:56 +0200

    Challenge - Scenario 1 – Mapping Step Definitions

---
+++ src/test/java/stepDefinitions/Login_Steps.java
@@ -0,0 +1,28 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Login_Steps {
+    @Given("I access the webdriver university login page")
+    public void i_access_the_webdriver_university_login_page() {
+    }
+
+    @When("I enter a username {string}")
+    public void i_enter_a_username(String string) {
+    }
+
+    @And("I enter a password {}")
+    public void i_enter_a_password(String password) {
+    }
+
+    @And("I click on the login button")
+    public void i_click_on_the_login_button() {
+    }
+
+    @Then("I should be presented with the successful login message")
+    public void i_should_be_presented_with_the_successful_login_message() {
+    }
+}
