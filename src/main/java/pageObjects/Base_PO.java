95f7dba59b79c241ba6f5d2d696dd1983ce26da9
jlferrete <joseluisferrete@gmail.com>
2022-04-15 09:38:41 +0200

    Base Page & Custom Commands - Part 3/3

--- src/main/java/pageObjects/Base_PO.java
+++ src/main/java/pageObjects/Base_PO.java
@@ -4,6 +4,7 @@ import driver.DriverFactory;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -32,5 +33,20 @@ public class Base_PO {
     public void sendKeys(By by, String textToType) {
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
         wait.until(ExpectedConditions.elementToBeClickable(by)).sendKeys(textToType);
+    }
+
+    public void sendKeys(WebElement element, String textToType) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).sendKeys(textToType);
+    }
+
+    public void waitForWebElementAndClick(By by) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
+
+    public void waitForWebElementAndClick(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
     }
 }
--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -60,7 +60,7 @@ public class Contact_Us_Steps extends Base_PO {
 
     @And("I click on the submit button")
     public void i_click_on_the_submit_button() {
-        driver.findElement(By.xpath("//input[@value=\"SUBMIT\"]")).click();
+        waitForWebElementAndClick(By.xpath("//input[@value=\"SUBMIT\"]"));
     }
 
     @Then("I should be presented with a successful contact us submission message")
